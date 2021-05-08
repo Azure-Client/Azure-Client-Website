7ecffd140ef96e4cfc2d5bf916a68ec5dcfc6049
Jeremiah <>
2021-05-08 04:16:24 +0000

    add server

--- server.py
+++ server.py
@@ -1,1 +1,9 @@
-...
+from flask import Flask
+from generators import *
+app = Flask('app')
+
+@app.route('/')
+def homepage():
+  return "Todo: Finish Homepage"
+
+app.run(host='0.0.0.0', port=8080)
